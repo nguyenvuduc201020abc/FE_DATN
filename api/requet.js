54bfaa09566a7c99f14773f3afb4b909018cc647
nguyenvuduc201020abc <you@example.com>
2023-08-08 10:01:40 +0700

    cc

--- api/requet.js
+++ api/requet.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-export const BASE_URL = 'http://103.163.118.131:8080'
+export const BASE_URL = 'http://127.0.0.1:8080'
 //export const BASE_URL = 'http://103.229.41.235:6243/api/'
 
 export const request = async () => {return await axios.create({
